ae318b91fdb00fd8e293a8cf9f9e2e7c4c95eaed
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-09-07 13:36:25 -0300

    build: adding the product repository

---
+++ ShoppingCart.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Api.Data;
+using ShoppingCart.Api.Entities;
+using ShoppingCart.Api.Repositories.Contracts;
+using System.Runtime.InteropServices;
+
+namespace ShoppingCart.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShoppingCartDbContext shoppingCartDbContext;
+
+        public ProductRepository(ShoppingCartDbContext shoppingCartDbContext)
+        {
+            this.shoppingCartDbContext = shoppingCartDbContext;
+        }
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await this.shoppingCartDbContext.ProductCategories.ToListAsync();
+            return categories;
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await this.shoppingCartDbContext.Products.ToListAsync();
+            return products;
+        }
+    }
+}
