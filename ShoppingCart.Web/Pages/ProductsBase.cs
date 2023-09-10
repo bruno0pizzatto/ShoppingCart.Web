6dc2a8301d31e7f3322303e85073f3ef1b23c8d8
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-09-10 19:07:59 -0300

    build: creating the blazor product page part1

---
+++ ShoppingCart.Web/Pages/ProductsBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShoppingCart.Models.Dtos;
+using ShoppingCart.Web.Services.Contratcs;
+
+namespace ShoppingCart.Web.Pages
+{
+    public class ProductsBase : ComponentBase
+    {
+        [Inject]
+        public IProductService ProductService { get; set; }
+
+        public IEnumerable<ProductDto> Products { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
---
+++ ShoppingCart.Web/Services/Contratcs/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using ShoppingCart.Models.Dtos;
+
+namespace ShoppingCart.Web.Services.Contratcs
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetItems();
+    }
+}
