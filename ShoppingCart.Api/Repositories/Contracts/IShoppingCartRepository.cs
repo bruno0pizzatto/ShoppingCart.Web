60b2d5e5e3741cf8c52a1bdfc7c15d71f8aafeff
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-09-12 09:32:18 -0300

    build: starting the implementation of ShoppingCart Entity

---
+++ ShoppingCart.Api/Repositories/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShoppingCart.Api.Entities;
+using ShoppingCart.Models.Dtos;
+using ShoppingCart.Models.DTOs;
+
+namespace ShoppingCart.Api.Repositories.Contracts
+{
+    public interface IShoppingCartRepository
+    {
+        Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto);
+        Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdateDto);
+        Task DeleteItem(int id);
+        Task<CartItem> GetItem(int id);
+        Task<IEnumerable<CartItem>> GetItems(int userId);
+
+    }
+}
---
+++ ShoppingCart.Api/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,35 @@
+﻿using ShoppingCart.Api.Entities;
+using ShoppingCart.Api.Repositories.Contracts;
+using ShoppingCart.Models.Dtos;
+using ShoppingCart.Models.DTOs;
+
+namespace ShoppingCart.Api.Repositories
+{
+    public class ShoppingCartRepository : IShoppingCartRepository
+    {
+        public Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CartItem>> GetItems(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
