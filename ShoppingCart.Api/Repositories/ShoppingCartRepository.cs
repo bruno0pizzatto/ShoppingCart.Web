e501870a4a302c617443f71433a467e40dbfbb93
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-09-12 09:41:43 -0300

    build: Implementing the AddItem into Shopping cart Repository

--- ShoppingCart.Api/Repositories/ShoppingCartRepository.cs
+++ ShoppingCart.Api/Repositories/ShoppingCartRepository.cs
@@ -1,4 +1,6 @@
-﻿using ShoppingCart.Api.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Api.Data;
+using ShoppingCart.Api.Entities;
 using ShoppingCart.Api.Repositories.Contracts;
 using ShoppingCart.Models.Dtos;
 using ShoppingCart.Models.DTOs;
@@ -7,9 +9,39 @@ namespace ShoppingCart.Api.Repositories
 {
     public class ShoppingCartRepository : IShoppingCartRepository
     {
-        public Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto)
+        private readonly ShoppingCartDbContext shoppingCartDbContext;
+
+        public ShoppingCartRepository(ShoppingCartDbContext shoppingCartDbContext)
         {
-            throw new NotImplementedException();
+            this.shoppingCartDbContext = shoppingCartDbContext;
+        }
+
+        private async Task<bool> CartItemExists(int cartId, int productId)
+        {
+            return await this.shoppingCartDbContext.CartItems.AnyAsync(c => c.CartId == cartId 
+                                                                         && c.ProductId == productId);
+        }
+        public async Task<CartItem> AddItem(CartItemToAddDto cartItemToAddDto)
+        {
+            if(await CartItemExists(cartItemToAddDto.CartId, cartItemToAddDto.ProductId) == false)
+            {
+                var item = await (from product in this.shoppingCartDbContext.Products
+                                  where product.Id == cartItemToAddDto.ProductId
+                                  select new CartItem
+                                  {
+                                      CartId = cartItemToAddDto.CartId,
+                                      ProductId = product.Id,
+                                      Qty = cartItemToAddDto.Qty
+                                  }).SingleOrDefaultAsync();
+                if (item != null)
+                {
+                    var result = await this.shoppingCartDbContext.CartItems.AddAsync(item);
+                    await this.shoppingCartDbContext.SaveChangesAsync();
+                    return result.Entity;
+                }
+            }
+            
+            return null;
         }
 
         public Task DeleteItem(int id)
