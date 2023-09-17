9f6da5ffc010a14d4398af18dfac452d9fa5a04a
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-09-17 18:11:47 -0300

    build: implementing the shopping cart add item

---
+++ ShoppingCart.Web/Services/Contratcs/IShoppingCartService.cs
@@ -0,0 +1,10 @@
+﻿using ShoppingCart.Models.DTOs;
+
+namespace ShoppingCart.Web.Services.Contratcs
+{
+    public interface IShoppingCartService
+    {
+        Task<IEnumerable<CartItemDto>> GetItems(int userId);
+        Task<CartItemDto> AddItem(CartItemToAddDto cartItemToAddDto);
+    }
+}
