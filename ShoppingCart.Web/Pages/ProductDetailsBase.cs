6537022fc246b658d49e8888d69394817634ded3
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-09-21 09:12:16 -0300

    build: building shoppingcart razor view and found bugs

--- ShoppingCart.Web/Pages/ProductDetailsBase.cs
+++ ShoppingCart.Web/Pages/ProductDetailsBase.cs
@@ -16,6 +16,9 @@ namespace ShoppingCart.Web.Pages
         [Inject]
         public IShoppingCartService ShoppingCartService { get; set; }
 
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }    
+
         public ProductDto Product { get; set; }
 
         public string ErrorMessage { get; set; }
@@ -37,6 +40,7 @@ namespace ShoppingCart.Web.Pages
             try
             {
                 var cartItemDto = await ShoppingCartService.AddItem(cartItemToAddDto);
+                NavigationManager.NavigateTo("/ShoppingCart");
             }
             catch (Exception)
             {
